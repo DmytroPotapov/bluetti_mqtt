1d2d3c00bb925d123dc1fd9fee8761228fabeae1
DmytroPotapov <dev.potapov@mailserver.com>
2024-04-15 19:48:26 +0300

    format

--- bluetti_mqtt/core/devices/ac180.py
+++ bluetti_mqtt/core/devices/ac180.py
@@ -37,9 +37,7 @@ class AC180(BluettiDevice):
         self.struct.add_uint_field("dc_output_power", 140)
         self.struct.add_uint_field("ac_output_power", 142)
         self.struct.add_uint_field("dc_input_power", 144)
-        self.struct.add_uint_field(
-            "ac_input_power", 146
-        )
+        self.struct.add_uint_field("ac_input_power", 146)
         # History
         # self.struct.add_decimal_field('power_generation', 154, 1)  # Total power generated since last reset (kwh)
         self.struct.add_decimal_field(
@@ -53,23 +51,23 @@ class AC180(BluettiDevice):
         #     for v in range(registers[k]):
         #         self.struct.add_uint_field('testI' + str(v+k), v+k)
         # Controls TEST
-        self.struct.add_bool_field('ac_output_on', 3007)
-        self.struct.add_bool_field('dc_output_on', 3008)
-        self.struct.add_bool_field('power_off', 3060)
-        self.struct.add_bool_field('eco_on', 3063)
-        self.struct.add_enum_field('ac_output_mode', 70, OutputMode)
-        self.struct.add_uint_field('internal_ac_voltage', 71)
-        self.struct.add_decimal_field('internal_current_one', 72, 1)
-        self.struct.add_uint_field('internal_power_one', 73)
-        self.struct.add_decimal_field('internal_ac_frequency', 74, 1)
-        self.struct.add_uint_field('internal_dc_input_voltage', 86)
-        self.struct.add_decimal_field('internal_dc_input_power', 87, 1)
-        self.struct.add_decimal_field('internal_dc_input_current', 88, 2)
-        self.struct.add_bool_field('ac_output_on', 48)
-        self.struct.add_bool_field('dc_output_on', 49)
-        
+        self.struct.add_bool_field("ac_output_on", 3007)
+        self.struct.add_bool_field("dc_output_on", 3008)
+        self.struct.add_bool_field("power_off", 3060)
+        self.struct.add_bool_field("eco_on", 3063)
+        self.struct.add_enum_field("ac_output_mode", 70, OutputMode)
+        self.struct.add_uint_field("internal_ac_voltage", 71)
+        self.struct.add_decimal_field("internal_current_one", 72, 1)
+        self.struct.add_uint_field("internal_power_one", 73)
+        self.struct.add_decimal_field("internal_ac_frequency", 74, 1)
+        self.struct.add_uint_field("internal_dc_input_voltage", 86)
+        self.struct.add_decimal_field("internal_dc_input_power", 87, 1)
+        self.struct.add_decimal_field("internal_dc_input_current", 88, 2)
+        self.struct.add_bool_field("ac_output_on", 48)
+        self.struct.add_bool_field("dc_output_on", 49)
+
         super().__init__(address, "AC180P", sn)
-        
+
     @property
     def polling_commands(self) -> List[ReadHoldingRegisters]:
         return [
